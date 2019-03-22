ef91a827fb57d022bb05bd9931c11882c083693f
Ariel <arielord@buffalo.edu>
2019-03-22 02:13:28 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -22,7 +22,12 @@ class Application
       end
     elsif req.path.match(/add/)
       search_term = req.params["q"]
-      @@cart << search_term unless handle_search(search_term).include?("Couldn't")
+      if !@@items.include?(search_term)
+        @@cart << search_term
+        resp.write "added #{search_term}"
+      else
+        resp.write "We don't have that item"
+      end
     else
       resp.write "Path Not Found"
     end
